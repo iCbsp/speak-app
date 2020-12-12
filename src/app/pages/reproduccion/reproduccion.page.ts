e303ce176b729d4efd086d722db92e917c59295a
iCbsp <carlos_bsp@hotmail.com>
2020-12-12 22:06:37 +0100

    Títulos: reproduciendo y escuchando

--- src/app/pages/reproduccion/reproduccion.page.ts
+++ src/app/pages/reproduccion/reproduccion.page.ts
@@ -13,6 +13,7 @@ import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
 export class ReproduccionPage implements OnInit {
   
   textoAReproducir = "";
+  reproduciendo = true;
 
   constructor(
     private route: ActivatedRoute, // Para recibir los parametros del Router
@@ -41,11 +42,17 @@ export class ReproduccionPage implements OnInit {
         locale: 'es-ES'
       });
       console.log("Successfully said " + this.textoAReproducir);
-      alert("El TTS ha terminado");
+      //alert("El TTS ha terminado");
+      this.reproduciendo = false;
     }
     catch(e){
-      console.log(e);
+      if(e == "cordova_not_available") console.log(e);
+      else alert("Ha surgido un error relacionado con el Text To Speech");
     }
+  }
+
+  estaReproduciendo(){
+    return this.reproduciendo;
   }
 
   pararTTS(){
