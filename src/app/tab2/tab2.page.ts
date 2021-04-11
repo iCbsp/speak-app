df9cb7907c5411d6f33ac70148daad2469767c37
iCbsp <carlos_bsp@hotmail.com>
2021-04-11 20:02:47 +0200

    Cambio en tabs para que todas sean personalizables

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -12,11 +12,11 @@ import { Platform } from '@ionic/angular';
 // Para actualizar la vista
 import { ChangeDetectorRef } from '@angular/core';
 
-// Popover
+// Popovers
 import { PopoverController } from '@ionic/angular';
 import { AccionPopoverPage } from 'src/app/components/accion-popover/accion-popover.page';
 
-import { TiposAcciones } from 'src/app/enumerations';
+import { ModoAccion, TiposAcciones } from 'src/app/enumerations';
 
 @Component({
   selector: 'app-tab2',
@@ -26,6 +26,8 @@ import { TiposAcciones } from 'src/app/enumerations';
 export class Tab2Page {
 
   acciones = [];
+  idAccionesSeleccionadas = [];
+  modoGrupo = false;
 
   constructor(
     public alertController: AlertController, // Alertas - Prompt
@@ -35,6 +37,7 @@ export class Tab2Page {
     private popover:PopoverController
   ){}
 
+  
   ngOnInit() {
     if(!this.platform.is('desktop')){
       this.databaseService.lista.subscribe((ready)=>{
@@ -44,6 +47,7 @@ export class Tab2Page {
       });
       this.databaseService.cambio.subscribe(()=>{
         this.consigueAcciones();
+        // alert("Cambio en la base de datos");
       });
     }
   }
@@ -59,13 +63,12 @@ export class Tab2Page {
     });
   }
   
-  createPopover(accion){
-    if(!this.platform.is('desktop') && accion){
+  crearAccionPopover(){
+    if(!this.platform.is('desktop')){
       this.popover.create({
       component:AccionPopoverPage,
-      cssClass: 'accionPopover',
       componentProps: {
-        accion: accion
+        modoAccion: ModoAccion.crear
       },
       showBackdrop: true
       }).then((popoverElement)=>{
@@ -74,5 +77,89 @@ export class Tab2Page {
     }
   }
 
+  async abrirAccionPopover(accionSeleccionada){
+    if(!this.modoGrupo){
+      if(!this.platform.is('desktop') && accionSeleccionada){
+        const popover = await this.popover.create({
+        component:AccionPopoverPage,
+        cssClass: 'accionPopover',
+        componentProps: {
+          accion: accionSeleccionada,
+          modoAccion: ModoAccion.ver
+        },
+        showBackdrop: true
+        });
+
+        popover.onDidDismiss().then((result) => {
+          //alert(result.data);
+        });
+        
+        return await popover.present();
+      } else alert("La función no es compatible con este dispositivo");
+    } else {
+      let estabaSeleccioada = false;
+      for(let accion = 0; accion < this.idAccionesSeleccionadas.length; accion++){
+        if(this.idAccionesSeleccionadas[accion] == accionSeleccionada.id){
+          this.idAccionesSeleccionadas.splice(accion, 1);
+          estabaSeleccioada = true;
+          break;
+        }
+      }
+      if(!estabaSeleccioada) this.idAccionesSeleccionadas.push(accionSeleccionada.id);
+    }
+  }
+
+  obtenIndiceSeleccionada(accionPreguntada){
+    for(let indiceAccion = 0; indiceAccion < this.idAccionesSeleccionadas.length; indiceAccion++){
+      if(accionPreguntada.id == this.idAccionesSeleccionadas[indiceAccion]) return (indiceAccion + 1);
+    }
+  }
+
+  async borrarAccionesSeleccionadas(){
+    let accionesABorrar = "";
+
+    if(this.idAccionesSeleccionadas.length){
+      for(let indiceAccionesSeleccionadas = 0; indiceAccionesSeleccionadas < this.idAccionesSeleccionadas.length; indiceAccionesSeleccionadas++){
+        for(let indiceAcciones = 0; indiceAcciones < this.acciones.length; indiceAcciones++){
+          if(this.acciones[indiceAcciones].id == this.idAccionesSeleccionadas[indiceAccionesSeleccionadas]) accionesABorrar += "<br>'" + this.acciones[indiceAcciones].titulo + "'";
+        }
+      }
+    } else {
+      alert("Debe de seleccionar al menos una acción");
+      return;
+    }
+
+    const ventanaConfirmacionBorrarAccion = await this.alertController.create({
+      cssClass: 'ventanaConfirmacionBorrarAccion',
+      header: 'Borrar acciones',
+      message: '¿Seguro que quiere borrar las siguientes acciones?<br>' + accionesABorrar,
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancelar',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Confirmar',
+          role: 'confirmar',
+          handler: () => {
+            console.log('Confirm Ok');
+            this.databaseService.borraAcciones(this.idAccionesSeleccionadas).then(() => {
+              this.idAccionesSeleccionadas = new Array();
+            });
+          }
+        }
+      ]
+    });
+
+    await ventanaConfirmacionBorrarAccion.present();
+  }
+
+  conmutarModoGrupo(){
+    this.modoGrupo = !this.modoGrupo;
+    this.idAccionesSeleccionadas = new Array();
+  }
 
 }
