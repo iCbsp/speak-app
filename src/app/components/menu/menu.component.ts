61f90585d8335b511a7bfb17bb9a1bdf5214b66b
iCbsp <carlos_bsp@hotmail.com>
2021-05-02 00:36:23 +0200

    Cambio de color y TTS en respuesta

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { SQLiteObject } from '@ionic-native/sqlite/ngx';
+import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
 
 // Para conocer el dispositivo
 import { MenuController, Platform } from '@ionic/angular';
 
 // Base de datos
 import { DatabaseService } from 'src/app/services/databaseService';
+import { EmojiStringComponent } from '../emoji-string/emoji-string.component';
 
 @Component({
   selector: 'app-menu',
@@ -20,7 +22,9 @@ export class MenuComponent implements OnInit {
   constructor(
     private plt: Platform,
     private databaseService:DatabaseService,
-    private menuController: MenuController
+    private menuController: MenuController,
+    private tts: TextToSpeech,
+    private emojiString: EmojiStringComponent
   ) {}
 
   ngOnInit() {
@@ -50,13 +54,44 @@ export class MenuComponent implements OnInit {
   cambiaModoSimple(){
     this.modoSimpleActivado = !this.modoSimpleActivado;
     this.databaseService.cambiaModoSimple(this.modoSimpleActivado);
-    if(this.modoSimpleActivado) this.menuController.close();
+    if(this.modoSimpleActivado) {
+      this.diTTS('Modo simple activado');
+      this.menuController.close();
+    }
   }
 
   
   cambiaRespuesta(){
     this.respuestaActivada = !this.respuestaActivada;
+    if(this.respuestaActivada) this.diTTS('Respuesta activada');
+    else this.diTTS('Respuesta desactivada');
     this.databaseService.cambiaRespuesta(this.respuestaActivada);
   }
+
+
+// Metodos TTS
+async diTTS(texto: string):Promise<any>{
+  var textoSinEmoticonos = this.emojiString.removeEmojis(texto);
+  try{
+    await this.tts.speak({
+      text: textoSinEmoticonos,
+      locale: 'es-ES',
+      rate: 0.8
+    });
+  }
+  catch(e){
+    if(e == "cordova_not_available") console.log(e);
+  }
+}
+
+async pararTTS(){
+  try{
+    await this.tts.speak("");
+  }
+  catch(e){
+    if(e == "cordova_not_available") console.log(e);
+    else alert("pararTTS: Ha surgido un error relacionado con el Text To Speech");
+  }
+}
 
 }
